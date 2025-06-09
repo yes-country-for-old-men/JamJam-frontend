8179672bb39205a41d5226f48f487f5f85728d14
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-06-09 10:31:12 +0900

    chore: src 폴더 내 디렉토리 별칭 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,4 +12,19 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: [
+      { find: '@assets', replacement: path.resolve(__dirname, 'src/assets') },
+      { find: '@atoms', replacement: path.resolve(__dirname, 'src/atoms') },
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, 'src/components'),
+      },
+      { find: '@hooks', replacement: path.resolve(__dirname, 'src/hooks') },
+      { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
+      { find: '@styles', replacement: path.resolve(__dirname, 'src/styles') },
+      { find: '@type', replacement: path.resolve(__dirname, 'src/types') },
+      { find: '@utils', replacement: path.resolve(__dirname, 'src/utils') },
+    ],
+  },
 });
