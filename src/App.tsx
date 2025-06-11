394effb6e208e270b98c5d2148e4d8015f823c6b
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-06-12 06:35:36 +0900

    chore: 전역 스타일 정의 및 reset css 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { ThemeProvider } from '@emotion/react';
 import theme from '@styles/theme';
+import GlobalStyle from '@styles/GlobalStyle';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <div />
+      <GlobalStyle />
     </ThemeProvider>
   );
 };
