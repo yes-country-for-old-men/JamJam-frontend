0cf6e2ce2491d9f54e7ad4ea2f2ab9439da77218
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-06-15 07:27:59 +0900

    feat: Header 컴포넌트 구현

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,95 @@
+import { useNavigate } from 'react-router-dom';
+import styled from '@emotion/styled';
+import { motion } from 'framer-motion';
+import LogoSVG from '@assets/logo.svg?react';
+
+const ANIMATION_VARIANTS = {
+  button: {
+    rest: { opacity: 1 },
+    hover: { opacity: 0.7 },
+  },
+} as const;
+
+const Container = styled.header`
+  position: sticky;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  top: 0;
+  width: 100vw;
+  height: 80px;
+  background-color: ${(props) => props.theme.COLORS.BACKGROUND};
+  padding: 0 24px;
+  z-index: 11;
+`;
+
+const HeaderContent = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 16px;
+`;
+
+const LogoButton = styled.button`
+  display: flex;
+  align-items: center;
+  padding: 0;
+  cursor: pointer;
+`;
+
+const SignInButton = styled(motion.button)`
+  display: flex;
+  color: ${(props) => props.theme.COLORS.LABEL_PRIMARY};
+  font-size: 14px;
+  font-weight: 600;
+  padding: 8px 12px;
+  cursor: pointer;
+`;
+
+const SignUpButton = styled(motion.button)`
+  display: flex;
+  background-color: ${(props) => props.theme.COLORS.JAMJAM_PRIMARY[1]};
+  color: white;
+  font-size: 14px;
+  font-weight: 700;
+  border-radius: 8px;
+  padding: 8px 12px;
+  cursor: pointer;
+`;
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate('/');
+  };
+
+  return (
+    <Container>
+      <HeaderContent>
+        <LogoButton onClick={handleLogoClick}>
+          <LogoSVG />
+        </LogoButton>
+      </HeaderContent>
+      <HeaderContent>
+        <SignInButton
+          variants={ANIMATION_VARIANTS.button}
+          initial="rest"
+          whileHover="hover"
+          transition={{ type: 'tween', duration: 0.2 }}
+        >
+          로그인
+        </SignInButton>
+        <SignUpButton
+          variants={ANIMATION_VARIANTS.button}
+          initial="rest"
+          whileHover="hover"
+          transition={{ type: 'tween', duration: 0.2 }}
+        >
+          회원가입
+        </SignUpButton>
+      </HeaderContent>
+    </Container>
+  );
+};
+
+export default Header;
