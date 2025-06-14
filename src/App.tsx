b3b48b98c82bc8f04f644466d0c6c98cdab951d3
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-06-15 07:37:36 +0900

    feat: App 컴포넌트에 라우터 설정 추가
    - Layout 컴포넌트 기반의 중첩 라우팅 구성 및 Main 페이지 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from '@emotion/react';
 import theme from '@styles/theme';
 import GlobalStyle from '@styles/GlobalStyle';
+import Layout from '@components/Layout';
+import Main from '@pages/Main';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Main />} />
+          </Route>
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 };
