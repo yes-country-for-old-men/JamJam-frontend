2102db660df1f67673512a2f327a26b0cb61bb7a
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-06-15 14:20:06 +0900

    feat: 공통 Modal 컴포넌트 구현 및 적용
    - App.tsx에서 jotai Provider와 Modal 컴포넌트를 추가해 모달 전역 상태 관리

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'jotai';
 import { ThemeProvider } from '@emotion/react';
 import theme from '@styles/theme';
 import GlobalStyle from '@styles/GlobalStyle';
 import Layout from '@components/Layout';
+import Modal from '@components/Modal';
 import Main from '@pages/Main';
 
 const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Main />} />
-          </Route>
-        </Routes>
-      </Router>
-    </ThemeProvider>
+    <Provider>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Main />} />
+            </Route>
+          </Routes>
+        </Router>
+        <Modal />
+      </ThemeProvider>
+    </Provider>
   );
 };
 
