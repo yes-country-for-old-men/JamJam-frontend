7a44a5644891d6288a1cbccdf726cfcbdf9a72e8
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-06-19 17:15:14 +0900

    feat: Category 페이지 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Layout from '@components/Layout';
 import Modal from '@components/Modal';
 import Main from '@pages/Main';
 import SignUp from '@pages/SignUp';
+import Category from '@pages/Category';
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Layout />}>
               <Route index element={<Main />} />
+              <Route path="/category/:categoryId" element={<Category />} />
             </Route>
             <Route path="/signup" element={<SignUp />} />
           </Routes>
