2ae8701a8b3adc359486b20e66e0dc1fbaf1479d
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-06-23 03:26:57 +0900

    feat: User 타입 정의

---
+++ src/types/User.ts
@@ -0,0 +1,10 @@
+export default interface User {
+  name: string;
+  nickname: string;
+  phoneNumber: string;
+  loginId: string;
+  birth: string;
+  role: 'PROVIDER' | 'CLIENT';
+  gender: 'MALE' | 'FEMALE';
+  profileUrl: string;
+}
