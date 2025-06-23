d921ec4e015fb531b9f39fa25f7b59b1d41aaa81
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-06-23 10:36:44 +0900

    chore: Vite 개발 서버 프록시 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import svgrPlugin from 'vite-plugin-svgr';
@@ -6,43 +6,55 @@ import mkcert from 'vite-plugin-mkcert';
 import fs from 'fs';
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  server: {
-    ...(command === 'serve' && {
-      https: {
-        key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
-        cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
+export default defineConfig(({ mode, command }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_SERVER_URL,
+          changeOrigin: true,
+          secure: true,
+          ws: true,
+        },
       },
-    }),
-  },
-  plugins: [
-    react({
-      jsxImportSource: '@emotion/react',
-      babel: {
-        plugins: ['@emotion/babel-plugin'],
-      },
-    }),
-    svgrPlugin(),
-    mkcert(),
-  ],
-  resolve: {
-    alias: [
-      { find: '@apis', replacement: path.resolve(__dirname, 'src/apis') },
-      { find: '@assets', replacement: path.resolve(__dirname, 'src/assets') },
-      {
-        find: '@components',
-        replacement: path.resolve(__dirname, 'src/components'),
-      },
-      {
-        find: '@constants',
-        replacement: path.resolve(__dirname, 'src/constants'),
-      },
-      { find: '@hooks', replacement: path.resolve(__dirname, 'src/hooks') },
-      { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
-      { find: '@stores', replacement: path.resolve(__dirname, 'src/stores') },
-      { find: '@styles', replacement: path.resolve(__dirname, 'src/styles') },
-      { find: '@type', replacement: path.resolve(__dirname, 'src/types') },
-      { find: '@utils', replacement: path.resolve(__dirname, 'src/utils') },
+      ...(command === 'serve' && {
+        https: {
+          key: fs.readFileSync(path.resolve(__dirname, 'localhost-key.pem')),
+          cert: fs.readFileSync(path.resolve(__dirname, 'localhost.pem')),
+        },
+      }),
+    },
+    plugins: [
+      react({
+        jsxImportSource: '@emotion/react',
+        babel: {
+          plugins: ['@emotion/babel-plugin'],
+        },
+      }),
+      svgrPlugin(),
+      mkcert(),
     ],
-  },
-}));
+    resolve: {
+      alias: [
+        { find: '@apis', replacement: path.resolve(__dirname, 'src/apis') },
+        { find: '@assets', replacement: path.resolve(__dirname, 'src/assets') },
+        {
+          find: '@components',
+          replacement: path.resolve(__dirname, 'src/components'),
+        },
+        {
+          find: '@constants',
+          replacement: path.resolve(__dirname, 'src/constants'),
+        },
+        { find: '@hooks', replacement: path.resolve(__dirname, 'src/hooks') },
+        { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
+        { find: '@stores', replacement: path.resolve(__dirname, 'src/stores') },
+        { find: '@styles', replacement: path.resolve(__dirname, 'src/styles') },
+        { find: '@type', replacement: path.resolve(__dirname, 'src/types') },
+        { find: '@utils', replacement: path.resolve(__dirname, 'src/utils') },
+      ],
+    },
+  };
+});
