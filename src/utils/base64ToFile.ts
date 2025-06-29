369a21772b4bbb0cbd160c15a11ef5aea7f9d5a2
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-06-29 22:25:11 +0900

    feat: base64 문자열을 File 객체로 변환하는 유틸리티 함수 추가

---
+++ src/utils/base64ToFile.ts
@@ -0,0 +1,17 @@
+const base64ToFile = (
+  base64String: string,
+  filename: string = 'thumbnail.png',
+): File => {
+  const base64Data = base64String.replace(/^data:image\/[a-z]+;base64,/, '');
+
+  const byteCharacters = atob(base64Data);
+  const byteNumbers = new Array(byteCharacters.length);
+  for (let i = 0; i < byteCharacters.length; i += 1) {
+    byteNumbers[i] = byteCharacters.charCodeAt(i);
+  }
+  const byteArray = new Uint8Array(byteNumbers);
+
+  return new File([byteArray], filename, { type: 'image/png' });
+};
+
+export default base64ToFile;
