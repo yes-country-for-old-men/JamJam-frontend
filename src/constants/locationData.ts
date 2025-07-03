4f53792bb7fed3a155496d8e3b28aa8cb154fe75
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-07-04 08:24:24 +0900

    chore: 기술/지역 선택용 상수 데이터 추가

---
+++ src/constants/locationData.ts
@@ -0,0 +1,21 @@
+const LOCATIONS = [
+  { id: 1, name: '서울특별시' },
+  { id: 2, name: '인천광역시' },
+  { id: 3, name: '경기도' },
+  { id: 4, name: '부산광역시' },
+  { id: 5, name: '울산광역시' },
+  { id: 6, name: '경상남도' },
+  { id: 7, name: '대구광역시' },
+  { id: 8, name: '경상북도' },
+  { id: 9, name: '대전광역시' },
+  { id: 10, name: '세종특별자치시' },
+  { id: 11, name: '충청남도' },
+  { id: 12, name: '충청북도' },
+  { id: 13, name: '광주광역시' },
+  { id: 14, name: '전라남도' },
+  { id: 15, name: '전북특별자치도' },
+  { id: 16, name: '강원특별자치도' },
+  { id: 17, name: '제주특별자치도' },
+];
+
+export default LOCATIONS;
