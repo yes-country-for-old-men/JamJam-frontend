840c762bc2a071c6218c4cf177d6104319d0be7a
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-07-05 23:59:57 +0900

    feat: 로딩 스피너 컴포넌트 구현

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,40 @@
+import styled from '@emotion/styled';
+import { keyframes } from '@emotion/react';
+
+const rotateAnimation = keyframes`
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+`;
+
+const Spinner = styled.div<{ size?: number }>`
+  position: relative;
+  display: block;
+  height: 56px;
+  width: 56px;
+  background: conic-gradient(
+    from 90deg at 50% 50%,
+    transparent 0deg,
+    ${(props) => props.theme.COLORS.JAMJAM_PRIMARY[1]} 360deg
+  );
+  border-radius: 50%;
+  animation: ${rotateAnimation} 1s infinite linear;
+  mask: radial-gradient(circle at center, transparent 20px, black 20px);
+
+  &::after {
+    content: '';
+    position: absolute;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    height: 8px;
+    width: 8px;
+    background-color: ${(props) => props.theme.COLORS.JAMJAM_PRIMARY[1]};
+    border-radius: 8px;
+  }
+`;
+
+export default Spinner;
