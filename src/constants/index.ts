b60d2ff54438173303071fbc3550069af7279b09
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-12-06 18:51:28 +0900

    feat: 공통 상수 중앙 export 파일 추가

---
+++ src/constants/index.ts
@@ -0,0 +1,8 @@
+export { default as BANK_CODE_MAP } from './bankCodes';
+export { default as LOCATIONS } from './locations';
+export {
+  default as SERVICE_CATEGORIES,
+  CATEGORY_IDS,
+} from './serviceCategories';
+export { default as SKILLS } from './skills';
+export { default as SLIDE_IMAGES } from './slideImages';
