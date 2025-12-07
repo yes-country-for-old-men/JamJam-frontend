93f3e8a101692c6157418a4709e6ce01d2987950
itwillbeoptimal <itwillbeoptimal@gmail.com>
2025-12-07 15:09:47 +0900

    refactor: 사용자 정보 쿼리 훅에서 통합된 user API 사용

--- src/hooks/queries/useUserInfoQuery.ts
+++ src/hooks/queries/useUserInfoQuery.ts
@@ -1,14 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 import { AxiosError } from 'axios';
 import type User from '@type/User';
-import { fetchUserInfo } from '@apis/user';
+import { getUserInfo } from '@apis/user';
 
 const useUserInfoQuery = () => {
   const hasToken = !!localStorage.getItem('accessToken');
 
   return useQuery<User, AxiosError>({
     queryKey: ['userInfo'],
-    queryFn: fetchUserInfo,
+    queryFn: getUserInfo,
     enabled: hasToken,
     staleTime: 5 * 60 * 1000,
     retry: false,
